17fd671bbaad4255bfbf2f98d3da4bb7518cbb65
Dheepanbalaji <dheepanbalajicse@gmail.com>
2021-09-13 11:37:14 +0530

    to do app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import {Guid} from "guid-typescript";
+import { Todo } from './models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular5';
+  'todos' : Todo[] = [
+    new Todo(Guid.create(),'Wash car', false),
+    new Todo(Guid.create(),'Buy Groceries', false),
+  ]
+
+  onSubmit(form:NgForm)
+  {
+    let todo=new Todo(Guid.create(),form.value.title,false);
+    this.todos.push(todo);
+    form.resetForm();
+  }
+  onComplete(id:Guid)
+  {
+      let todo=this.todos.filter(x=>x.id===id)[0];
+      todo.isComplete =true;
+  }
+  onDelete(id:Guid)
+  {
+    let todo=this.todos.filter(x=>x.id===id)[0];
+    let index = this.todos.indexOf(todo,0);
+    if(index>-1)
+    {
+      this.todos.splice(index,1);
+    }
+  }
 }
